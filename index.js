f5787cf41511c2ccaf00368ea2cfa9e51d99a5c1
Robert Laws <roblaws@me.com>
2024-01-07 13:22:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const posts = require('./posts');
+
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from the API!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000');
+});
