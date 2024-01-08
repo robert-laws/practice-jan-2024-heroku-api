d81f636fe51f926f32850d49ad824b4f00a8d1ac
Robert Laws <roblaws@me.com>
2024-01-08 12:35:02 +0300

    add :id route

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.get('/', (req, res) => {
   res.send('Hello from the API!');
 });
 
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = posts.find((post) => post._id === id);
+
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404).send({ error: 'Post not found' });
+  }
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
